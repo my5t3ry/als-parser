6a93ed820c97530e089214e71146d0ee730e4a9b
my5t3ry <sash@mischkonsum.org>
2017-09-09 00:46:12 +0200

    added sl4j

--- src/main/java/de/my5t3ry/als_parser/domain/AbletonProject/DeprecatedAbletonProject.java
+++ src/main/java/de/my5t3ry/als_parser/domain/AbletonProject/DeprecatedAbletonProject.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
  * created by: sascha.bast
  * since: 08.09.17
  */
-public class DeprecatedAbletonProject extends  AbletonProject{
+public class DeprecatedAbletonProject extends AbletonProject{
 
     public DeprecatedAbletonProject() {
         this.internalDevices = new ArrayList<>();
